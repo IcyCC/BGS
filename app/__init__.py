12e17688fd56a1a596bc8491ef8c6f615397c337
zhandong <1468767640@qq.com>
2018-03-10 17:17:12 +0800

    new doc

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -11,10 +12,13 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    CORS(app, supports_credentials=True)
+
     db.init_app(app)
+
+    app.template_folder='/home/zhandong/PycharmProjects/accu_chek/templates'
 
     from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
-
 
     return app

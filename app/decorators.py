f3e30a116bf34a24ca570b03230fd3ed479536e9
zhandong <1468767640@qq.com>
2018-03-13 21:37:26 +0800

    change

--- app/decorators.py
+++ app/decorators.py
@@ -4,8 +4,9 @@ def allow_cross_domain(fun):
     @wraps(fun)
     def wrapper_fun(*args, **kwargs):
         rst = make_response(fun(*args, **kwargs))
-        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
         rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        rst.headers['Access-Control-Allow-Credentials'] = 'true'
         allow_headers = "Referer,Accept,Origin,User-Agent"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
